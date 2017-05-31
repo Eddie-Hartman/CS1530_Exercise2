d67c7bbba0d58b38161be81fbaa3a087cd133ef7
Eddie Hartman <hartmane11@yahoo.com>
2017-05-31 14:57:51 -0400

    Added new file

---
+++ src/main/java/exercise2.java
@@ -0,0 +1,22 @@
+public static void main(String[] args()){
+
+	if(args.length()!=2){
+		System.out.println("Too few or too many arguments.");
+	}
+	else{
+		if(!args[1].equals("lazy")&&!args[1].equals("triangle")){
+			System.out.println("Incorrect first argument. Try lazy or triangle.");
+		}
+		else{
+			if(args[1].equals("lazy"){
+				tri(
+
+	
+	public void tri(int number){
+		
+	}
+	
+	public void fib(int number){
+		
+	}
+}
